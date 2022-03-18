d6667f06d98525f0bb484ce45a4b1459089d9541
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-03-18 10:28:48 +0300

    add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="div logo">
+          <h1>OMDb API</h1>
+          <p>The Open Movie Database 🍿</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
